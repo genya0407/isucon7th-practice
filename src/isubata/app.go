3fcdc4c27b7dbd871c377a38c13452d3cb387f2a
Yusuke Sangenya <longinus.eva@gmail.com>
2018-09-02 15:07:05 +0900

    Decrease fetch wait

--- src/isubata/app.go
+++ src/isubata/app.go
@@ -489,7 +489,7 @@ func fetchUnread(c echo.Context) error {
 	lastFetchedAt, ok := lastFetchedAtByUser[userID]
 	if ok {
 		now := time.Now()
-		shouldFetchedAt := lastFetchedAt.Add(time.Duration(9) * time.Second)
+		shouldFetchedAt := lastFetchedAt.Add(time.Duration(8) * time.Second)
 		if shouldFetchedAt.Before(now) {
 			// immediate return
 		} else {
