92ca91a2a87293d6d7751017b8881412043e7923
Yusuke Sangenya <longinus.eva@gmail.com>
2018-09-02 16:06:16 +0900

    add cnt to struct

--- src/isubata/types/ts.go
+++ src/isubata/types/ts.go
@@ -16,6 +16,7 @@ type ChannelInfo struct {
     ID          int64     `db:"id"`
     Name        string    `db:"name"`
     Description string    `db:"description"`
+    Cnt         int64      `db:"cnt"`
     UpdatedAt   time.Time `db:"updated_at"`
     CreatedAt   time.Time `db:"created_at"`
 }
